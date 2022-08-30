22ba84ec2977d40289f43321bd33dc4ce3f5438f
raselASrock <mdraselm413@gmail.com>
2022-08-30 10:38:29 +0600

    load-data-api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('App Added')
+const loadPhones = async () =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const response = await fetch(url)
+    const data = await response.json()
+    displayPhones(data.data)
+}
+
+const displayPhones = phones =>{
+    const phonesContainer = document.getElementById('phone-container')
+    phones.forEach(phone =>{
+        
+    })
+}
+
+loadPhones()
