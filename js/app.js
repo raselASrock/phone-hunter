9dcc27fbe4e6cb5215f328d9bcc095cc154b2a86
raselASrock <mdraselm413@gmail.com>
2022-08-30 11:59:31 +0600

    warning-added

--- js/app.js
+++ js/app.js
@@ -9,6 +9,18 @@ const displayPhones = phones =>{
     console.log(phones)
     const phonesContainer = document.getElementById('phone-container')
     phonesContainer.textContent = ''
+    // Display 10 phones Only
+    phones = phones.slice(0, 10)
+
+    // Display No Phones Found
+    const noPhone = document.getElementById('no-found-message')
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none')
+    }
+    else{
+        noPhone.classList.add('d-none')
+    }
+    // Display All Phones
     phones.forEach(phone =>{
         const phoneDiv = document.createElement('div')
         phoneDiv.classList.add('col')
@@ -19,7 +31,7 @@ const displayPhones = phones =>{
                 <h5 class="card-title fw-bold">${phone.phone_name}</h5>
                 <h6 class="text-muted" >Brand: ${phone.brand}</h6>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <h6 class="text-muted" >IMEI:${phone.slug}</h6>
+                <h6 class="text-muted" >Model:${phone.slug}</h6>
             </div>
         </div>
         `
@@ -36,4 +48,4 @@ document.getElementById('btn-search').addEventListener('click',function(){
 
 })
 
-loadPhones()
+// loadPhones()
